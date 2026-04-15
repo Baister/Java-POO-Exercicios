e07eecbd703cd9f7ca52193b5319d310018fb6ef
Gustavo <gustavobaister@gmail.com>
2026-04-15 19:44:48 -0300

    feat: criação de getters e setters

--- SistemaBancario/src/Conta.java
+++ SistemaBancario/src/Conta.java
@@ -1,5 +1,31 @@
 public abstract class Conta {
     private int numero;
     private double saldo;
-    
+
+    public int getNumero(){
+        return numero;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public boolean setNumero(int numero){
+        if(numero < 0){
+            return false;
+        }else{
+            this.numero = numero;
+            return true;
+        }
+    }
+    public boolean setSaldo(double saldo){
+        if(saldo < 0){
+            return false;
+        }else{
+            this.saldo = saldo;
+            return true;
+        }
+    }
+
+
+
 }
