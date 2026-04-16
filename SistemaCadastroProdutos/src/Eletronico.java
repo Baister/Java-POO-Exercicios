a3fe9ca4fa120b726ff1858531577dffc8d64a50
Gustavo <gustavobaister@gmail.com>
2026-04-15 21:31:31 -0300

    fix: arrumando override

--- SistemaCadastroProdutos/src/Eletronico.java
+++ SistemaCadastroProdutos/src/Eletronico.java
@@ -2,7 +2,7 @@ public class Eletronico  extends Produto{
     public Eletronico(String nome, double preco, String categoria){
         super(nome, preco, categoria);
     }
-
+    @Override
     public double calcularDesconto(){
         double desconto = 0.95;
         return getPreco() * desconto;
--- SistemaCadastroProdutos/src/Livro.java
+++ SistemaCadastroProdutos/src/Livro.java
@@ -2,7 +2,7 @@ public class Livro extends Produto {
     public Livro(String nome, double preco, String categoria){
         super(nome, preco, categoria);
     }
-
+    @Override
     public double calcularDesconto(){
         double desconto = 0.9; //Desconto de 10%
         return getPreco() * desconto;
